23fc6e0b8e9b6148bcc4e7693834026288430eb6
Gerardo Antonio <gerardo.antonio92@gmail.com>
2017-11-18 00:01:33 +0000

    ex5dot3 echoingindividual chars

---
+++ ex5dot3/uart.d
@@ -0,0 +1,9 @@
+uart.o: uart.c \
+ /home/gl/Desktop/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/stm32f10x.h \
+ /home/gl/Desktop/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/CoreSupport/core_cm3.h \
+ /home/gl/Desktop/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.h \
+ ../../template/STM32-Template/stm32f10x_conf.h \
+ /home/gl/Desktop/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_rcc.h \
+ /home/gl/Desktop/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_gpio.h \
+ /home/gl/Desktop/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_usart.h \
+ uart.h

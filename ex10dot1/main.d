7bd0232d802ff72bba76a3361c76fd5af1a3cb9d
Gerardo Antonio <gerardo.antonio92@gmail.com>
2017-12-25 15:01:22 +0000

    Done ex10dot1

---
+++ ex10dot1/main.d
@@ -0,0 +1,8 @@
+main.o: main.c \
+ /home/gl/Desktop/E/Hardware/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/stm32f10x.h \
+ /home/gl/Desktop/E/Hardware/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/CoreSupport/core_cm3.h \
+ /home/gl/Desktop/E/Hardware/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.h \
+ ../../template/STM32-Template/stm32f10x_conf.h \
+ /home/gl/Desktop/E/Hardware/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_rcc.h \
+ /home/gl/Desktop/E/Hardware/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_gpio.h \
+ /home/gl/Desktop/E/Hardware/STM32/book/STlibs/STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_tim.h
